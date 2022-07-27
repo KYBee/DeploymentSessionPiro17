9f23627f2e013d99908492c8c822aceacd3a639b
KYBee <kyb3634@naver.com>
2022-07-28 01:04:01 +0900

    :sparkles: Implement v1.0.0
    Implement v1.0.0

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,8 @@
-from django.contrib import admin
 from django.urls import path
+from .views import *
 
 app_name = "home"
 
 urlpatterns = [
-    path('', admin.site.urls),
+    path('', welcome_home, name="welcome_home"),
 ]
